486f9e13e32ea49af7b3a3083b8a54e7bf899087
Cesar <ccordero.007@gmail.cim>
2024-02-18 18:27:13 -0600

    Showing news in template html

--- app/news/views.py
+++ app/news/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 # Create your views here.
 def index(request):
-    print(dir(request))
-    return HttpResponse('Hello world')
+    # # print(dir(request))
+    # news = News.objects.all()
+    # res = '<h1>News</h1>'
+    # for item in news:
+    #     res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div><hr>\n'
+    # # return HttpResponse('Hello world')
+    # return HttpResponse(res)
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news':news,
+        'title':'News Index'
+    }
+    return render(request, template_name='news/index.html', context=context)
 
 def test(request):
     return HttpResponse('<h1>Test testing</h1>')
