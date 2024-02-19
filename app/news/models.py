dedb26966491781a633402b08d14d31dbc795e77
Cesar <ccordero.007@gmail.cim>
2024-02-18 22:22:11 -0600

    Category model added and link with news model as foreign key

--- app/news/models.py
+++ app/news/models.py
@@ -7,6 +7,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name="verbosenamenewsupdatedat")
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name="verbosenamenewsphoto", blank=True)
     is_published = models.BooleanField(default=True, verbose_name="verbosenamenewsispublish")
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -15,3 +16,14 @@ class News(models.Model):
         verbose_name = 'News'
         verbose_name_plural = 'News'
         ordering = ['-created_at']
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = 'Categories'
+        verbose_name_plural = 'Categories'
+        ordering = ['-title']
