1efb56009419cf87a6afd3b43ecd85e3a775db60
Cesar <ccordero.007@gmail.cim>
2024-02-19 12:09:15 -0600

    Custom tags in django template for sidebar

--- app/news/views.py
+++ app/news/views.py
@@ -13,16 +13,13 @@ def index(request):
     # return HttpResponse(res)
     # news = News.objects.order_by('-created_at')
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news':news,
         'title':'News Index',
-        'categories': categories
     }
     return render(request, template_name='news/index.html', context=context)
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news':news, 'categories':categories, 'category': category})
+    return render(request, 'news/category.html', {'news':news, 'category': category})
