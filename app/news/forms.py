e550b1c3fa49d6123fc59668a9e3dc6f07da6136
Cesar <ccordero.007@gmail.cim>
2024-02-20 09:04:45 -0600

    forms.ModelForm implemented in news forms

--- app/news/forms.py
+++ app/news/forms.py
@@ -1,8 +1,19 @@
 from django import forms
-from .models import Category
+from .models import Category, News
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Title news', widget=forms.TextInput(attrs={"class":"form-control"}))
-    content = forms.CharField(label="Conten news", required=False, widget=forms.Textarea(attrs={"class":"form-control","rows":5}))
-    is_published = forms.BooleanField(label="Is published?", initial=True)
-    category = forms.ModelChoiceField(empty_label="Select Category",queryset=Category.objects.all(), widget=forms.Select(attrs={"class":"form-control"}))
+# class NewsForm(forms.Form):
+    # title = forms.CharField(max_length=150, label='Title news', widget=forms.TextInput(attrs={"class":"form-control"}))
+    # content = forms.CharField(label="Conten news", required=False, widget=forms.Textarea(attrs={"class":"form-control","rows":5}))
+    # is_published = forms.BooleanField(label="Is published?", initial=True)
+    # category = forms.ModelChoiceField(empty_label="Select Category",queryset=Category.objects.all(), widget=forms.Select(attrs={"class":"form-control"}))
+    
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        # fields = '__all__'
+        fields = ['title','content','is_published','category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control'}),
+            'content': forms.Textarea(attrs={'class':'form-control'}),
+            'category': forms.Select(attrs={'class':'form-control'}),
+        }
